68699c017dd7649d1b33bc9324238297ac79cb45
ShMaksim <161884292+ShMaksim@users.noreply.github.com>
2024-05-15 21:32:30 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '6847350480:AAG8Bj_2rCa9mpwue2uZpKqbanxl8DYcuGQ'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'крона': 'CZK',
+    'euro': 'EUR',
+    'dollar': 'USD',
+    'rub': 'RUB',
+    'koruna': 'CZK'
+}
+
